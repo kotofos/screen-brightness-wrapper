f01a4a2e8f0a244b1485948a3606cf2dfab2ce0b
Konstantin Kudryavtsev <konstantin.kudryavtsev@neulion.com>
2016-09-12 19:15:51 +0700

    Move code to class

--- screen_bright_wrapper.py
+++ screen_bright_wrapper.py
@@ -30,6 +30,47 @@ def check_config(cfg):
             cfg[key] = def_value
 
 
+class MonitorController():
+
+    def __init__(self):
+
+        try:
+            with open('config.yaml', 'r') as ymfile:
+                self.config = yaml.load(ymfile)
+                logging.debug('config loaded')
+        except FileNotFoundError:
+            self.config = {}
+            logging.warning('no config found, using defaults')
+
+        check_config(self.config)
+
+        if self.config['debug']:
+            logging.basicConfig(level=logging.DEBUG)
+        else:
+            logging.basicConfig(level=logging.CRITICAL)
+
+        logging.debug(os.path.abspath(os.path.curdir))
+
+    def change_brightness(self, brightness_change):
+        old_brightness = self.config['brightness']
+        logging.debug('old brightness {}'.format(old_brightness))
+
+        logging.debug('brightness change {}'.format(brightness_change))
+        brightness = brightness_change + old_brightness
+        brightness = clamp_brightness(brightness)
+
+        logging.info('new brightness {}'.format(brightness))
+        if platform.system() == 'Windows':
+            subprocess.call(('ScreenBright.exe', '-set', 'brightness', str(brightness)))
+        elif platform.system() == 'Darwin':
+            subprocess.call(('ddcctl', '-d', '1', '-b', str(brightness)))
+
+
+        self.config['brightness'] = brightness
+        with open('config.yaml', 'w') as ymfile:
+            yaml.dump(self.config, ymfile)
+            logging.debug('config saved')
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     script_dir = os.path.dirname(os.path.realpath(__file__))
@@ -39,38 +80,5 @@ if __name__ == '__main__':
                             type=int, choices=range(-100, 101), default=0)
     args = arg_parser.parse_args()
 
-    try:
-        with open('config.yaml', 'r') as ymfile:
-            config = yaml.load(ymfile)
-            logging.debug('config loaded')
-    except FileNotFoundError:
-        config = {}
-        logging.warning('no config found, using defaults')
-
-    check_config(config)
-
-    if config['debug']:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.CRITICAL)
-
-    logging.debug(os.path.abspath(os.path.curdir))
-
-    old_brightness = config['brightness']
-    logging.debug('old brightness {}'.format(old_brightness))
-
-    logging.debug('brightness change {}'.format(args.brightness_change))
-    brightness = args.brightness_change + old_brightness
-    brightness = clamp_brightness(brightness)
-
-    logging.info('new brightness {}'.format(brightness))
-    if platform.system() == 'Windows':
-        subprocess.call(('ScreenBright.exe', '-set', 'brightness', str(brightness)))
-    elif platform.system() == 'Darwin':
-        subprocess.call(('ddcctl', '-d', '1', '-b', str(brightness)))
-
-
-    config['brightness'] = brightness
-    with open('config.yaml', 'w') as ymfile:
-        yaml.dump(config, ymfile)
-        logging.debug('config saved')
+    mn = MonitorController()
+    mn.change_brightness(args.brightness_change)
