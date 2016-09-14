9182fa23f5a1e981303f5982ed5ebf3fd3026fbf
(kotofos) <(ibelkov@gmail.com)>
2016-09-14 20:16:37 +0700

    Add set function. Refactor config code

--- screen_bright_wrapper.py
+++ screen_bright_wrapper.py
@@ -22,54 +22,67 @@ def clamp_brightness(val):
         return MIN_BRIGHTNESS
 
 
-def check_config(cfg):
-    keys = (('brightness', DEFAULT_BRIGHTNESS_VALUE),
-            ('debug', False))
-    for key, def_value in keys:
-        if cfg.get(key, None) is None:
-            cfg[key] = def_value
-
-
 class MonitorController:
     def __init__(self):
 
+        self.config_file = os.path.expanduser('~/.monitor_controller/config.yaml')
+
         try:
-            self.config_file = os.path.expanduser('~/.monitor_controller/config.yaml')
-            with open(self.config_file, 'r') as ymfile:
-                self.config = yaml.load(ymfile)
-                logging.debug('config loaded')
+            self.load_config()
         except FileNotFoundError:
             self.config = {}
             logging.warning('no config found, using defaults')
-
-        check_config(self.config)
+            self.init_config_file()
+        self.check_config()
 
         if self.config['debug']:
             logging.basicConfig(level=logging.DEBUG)
         else:
             logging.basicConfig(level=logging.CRITICAL)
 
-        logging.debug(os.path.abspath(os.path.curdir))
+        logging.debug('working dir: {}'.format(os.path.abspath(os.path.curdir)))
 
-    def change_brightness(self, brightness_change):
+    def set_brightness(self, brightness):
         old_brightness = self.config['brightness']
         logging.debug('old brightness {}'.format(old_brightness))
 
-        logging.debug('brightness change {}'.format(brightness_change))
-        brightness = brightness_change + old_brightness
+        logging.debug('new brihtness {}'.format(brightness))
         brightness = clamp_brightness(brightness)
 
-        logging.info('new brightness {}'.format(brightness))
         if platform.system() == 'Windows':
             subprocess.run(('ScreenBright.exe', '-set', 'brightness', str(brightness)))
         elif platform.system() == 'Darwin':
             subprocess.run(('ddcctl', '-d', '1', '-b', str(brightness)))
 
         self.config['brightness'] = brightness
-        os.makedirs(os.path.split(self.config_file)[0], exist_ok=True)
+        self.save_config()
+
+    def change_brightness(self, brightness_change):
+        old_brightness = self.config['brightness']
+
+        logging.debug('brightness change {}'.format(brightness_change))
+        brightness = brightness_change + old_brightness
+        self.set_brightness(brightness)
+
+    def check_config(self):
+        keys = (('brightness', DEFAULT_BRIGHTNESS_VALUE),
+                ('debug', False))
+        for key, def_value in keys:
+            if self.config.get(key, None) is None:
+                self.config[key] = def_value
+
+    def load_config(self):
+        with open(self.config_file, 'r') as ymfile:
+            self.config = yaml.load(ymfile)
+            logging.debug('config loaded {}'.format(self.config_file))
+
+    def save_config(self):
         with open(self.config_file, 'w') as ymfile:
             yaml.dump(self.config, ymfile)
             logging.debug('config saved')
+
+    def init_config_file(self):
+        os.makedirs(os.path.split(self.config_file)[0], exist_ok=True)
 
 
 if __name__ == '__main__':
